3f7e7cc203fd5d2752b61f1b0491371139effeec
Henrich Moraes <henrich.moraes@resultadosdigitais.com.br>
2017-10-23 15:42:26 -0200

    Adds support to SMTP config via environment variables

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -14,25 +14,38 @@ TRY_LOOP="20"
 : ${POSTGRES_PASSWORD:="airflow"}
 : ${POSTGRES_DB:="airflow"}
 
+: ${SMTP_FROM:="airflow@airflow.com"}
+: ${SMTP_USER:="airflow"}
+: ${SMTP_PASSWORD:="airflow"}
+: ${SMTP_PORT:="25"}
+: ${SMTP_HOST:="localhost"}
+
 : ${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
 
 # Load DAGs exemples (default: Yes)
 if [ "$LOAD_EX" = "n" ]; then
-    sed -i "s/load_examples = True/load_examples = False/" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s/load_examples = True/load_examples = False/" "$AIRFLOW_HOME"/airflow.cfg
 fi
 
 if [ "$REMOTE_LOG" = "true" ]; then
-    sed -i "s/remote_log_conn_id =/remote_log_conn_id = s3_default/" "$AIRFLOW_HOME"/airflow.cfg
-    sed -i "s|remote_base_log_folder =|remote_base_log_folder = $REMOTE_LOG_LOCATION|" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s/remote_log_conn_id =/remote_log_conn_id = s3_default/" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s|remote_base_log_folder =|remote_base_log_folder = $REMOTE_LOG_LOCATION|" "$AIRFLOW_HOME"/airflow.cfg
 fi
 
 # Install custome python package if requirements.txt is present
 if [ -e "/requirements.txt" ]; then
-    $(which pip) install --user -r /requirements.txt
+  $(which pip) install --user -r /requirements.txt
 fi
 
 # Update airflow config - Fernet key
 sed -i "s|\$FERNET_KEY|$FERNET_KEY|" "$AIRFLOW_HOME"/airflow.cfg
+
+# Update airflow config - STMP Config
+sed -i "s|smtp_mail_from = airflow@airflow.com|smtp_mail_from = $SMTP_FROM|" "$AIRFLOW_HOME"/airflow.cfg
+sed -i "s|# smtp_user = airflow|smtp_user = $SMTP_USER|" "$AIRFLOW_HOME"/airflow.cfg
+sed -i "s|# smtp_password = airflow|smtp_password = $SMTP_PASSWORD|" "$AIRFLOW_HOME"/airflow.cfg
+sed -i "s|smtp_port = 25|smtp_port = $SMTP_PORT|" "$AIRFLOW_HOME"/airflow.cfg
+sed -i "s|smtp_host = localhost|smtp_host = $SMTP_HOST|" "$AIRFLOW_HOME"/airflow.cfg
 
 # Wait for Postresql
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
